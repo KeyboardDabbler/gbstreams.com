5e51386cfe24625f5cad89c9dee1426b9b8d38ca
keyboarddabbler <keyboarddabbler@gmail.com>
2023-03-22 19:30:30 +1300

    feat: homepage

---
+++ components/FeaturesHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <nav class="flex justify-between p-2 px-4 xl:px-20 border-b-1 border-gray-400 border-opacity-40">
+            <div class="flex justify-between items-center w-full">
+                <div class="xl:w-1/3">
+                    <NuxtLink class="block max-w-max">
+                        <img class="h-9" src="~/assets/img/banner-light.png" alt="GBstreams logo">
+                    </NuxtLink>
+                </div>
+                <div class="hidden md:block xl:w-1/3">
+                    <div class="flex items-center justify-end">
+                        <NuxtLink to="/login"
+                            class="inline-block py-2 px-4 leading-5 decoration-yellow decoration-2 underline-offset-8 hover:underline bg-transparent font-medium rounded-md">
+                            Login Page
+                        </NuxtLink>
+                    </div>
+                </div>
+            </div>
+            <button class="self-center md:hidden">
+                <Icon name="ic:baseline-menu" size="35px" />
+            </button>
+        </nav>
+    </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+
+import FeaturesList from '~~/components/FeaturesList.vue';
+
+<template>
+    <div class="flex flex-col min-h-screen">
+        <FeaturesHeader />
+        <div class="flex-1 w-full flex flex-col">
+            <div class="relative flex-1 flex flex-col mx-auto max-w-8xl w-full h-full">
+                <FeaturesList />
+            </div>
+        </div>
+        <TheFooter />
+    </div>
+</template>
