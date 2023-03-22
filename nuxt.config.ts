c8ced8f6a9fdb4c5138de592d6601f59a40937ad
keyboarddabbler <keyboarddabbler@gmail.com>
2023-03-22 15:05:14 +1300

    init: nuxt-windicss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxt/content'
+        '@nuxt/content',
+        'nuxt-windicss',
+      ],
+      css: [
+        '@/resources/main.scss',
+        '@/resources/scrollbars.css',
       ],
       content: {
         // https://content.nuxtjs.org/api/configuration
       }
+
 })
---
+++ windi.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'windicss/helpers'
+// themes
+import defaultTheme from 'windicss/defaultTheme'
+
+export default defineConfig({
+  attributify: false,
+  extract: {
+    include: [
+      './components/**/*.{vue,js}',
+      './composables/**/*.{js,ts}',
+      './content/**/*.md',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './utils/**/*.{js,ts}',
+      './app.vue',
+    ],
+  },
+  theme: {
+    extend: {
+      colors: {
+        backg: '#111828',
+        forg: '#202938',
+        yellow: '#f4cf44',
+        yellowf: '#897931',
+        bluer: '#007aff',
+      },
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'login-pattern': "url('')",
+      },
+    },
+  },
+})
