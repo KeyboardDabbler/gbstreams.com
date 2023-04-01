4ac44b57b6cc7dbc6e49dc9f6c0262b4e9dd0ecf
keyboarddabbler <keyboarddabbler@gmail.com>
2023-04-01 18:09:46 +1300

    feat: create custom content styling

---
+++ components/content/ProseH6.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ id: string }>()
+</script>
+
+<template>
+  <h6 :id="id" class="relative mt-4 mb-1">
+    <a :href="`#${id}`" class="inline-flex items-center group">
+      <span class="hidden pr-2 text-yellow group-hover:inline-block">
+        #
+      </span>
+      <slot />
+    </a>
+  </h6>
+</template>
