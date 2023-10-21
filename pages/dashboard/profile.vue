48446932895506d75d055e82b516920e4247d0e0
keyboarddabbler <keyboarddabbler@gmail.com>
2023-10-21 17:39:54 +1300

    feat: profile page layout

---
+++ pages/dashboard/profile.vue
@@ -0,0 +1,21 @@
+<script>
+definePageMeta({
+    layout: "dashboard",
+});
+</script>
+
+<template>
+    <NuxtLayout>
+        <main class="container px-4 focus:outline-none">
+            <div class="mb-6">
+                <div class="max-w-8xl mx-auto px-4">
+                    <NavProfileHeader :button="'Edit Settings'" :buttonLink="'/dashboard/settings/general'"/>
+                    <StatsUserStats />
+                </div>
+            </div>
+            <div class="m-6 pt-6">
+                <StatsUserWatchHistory />
+            </div>
+        </main>
+    </NuxtLayout>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col min-h-screen">
-        <NavDefaultHeader :button="'Login Page'" :buttonLink="'/auth/login'" />
+        <NavDefaultHeader c />
         <div class="flex-1 w-full flex flex-col">
             <div class="relative flex-1 flex flex-col mx-auto max-w-8xl w-full h-full">
                 <SectionFeaturesList />
