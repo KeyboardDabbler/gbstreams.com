330890d5fd00bff99d148b4fc9c33ae99dbf8ff6
keyboarddabbler <keyboarddabbler@gmail.com>
2023-12-02 20:26:22 +1300

    fix: add login page link to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col min-h-screen">
-        <NavDefaultHeader c />
+        <NavDefaultHeader :button="'Login Page'" :buttonLink="'/auth/login'" />
         <div class="flex-1 w-full flex flex-col">
             <div class="relative flex-1 flex flex-col mx-auto max-w-8xl w-full h-full">
                 <SectionFeaturesList />
