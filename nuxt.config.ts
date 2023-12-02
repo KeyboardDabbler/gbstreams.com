8aa5c548c77ed7c53d162bbcf88e9086fc8a167a
keyboarddabbler <keyboarddabbler@gmail.com>
2023-12-02 20:28:57 +1300

    init: Add @sidebase/nuxt-auth to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-      '@nuxt/content',
-      'nuxt-windicss',
-      'nuxt-headlessui',
-      'nuxt-icon',
-      '@nuxt/image-edge',
-      '@formkit/nuxt',
-      "@nuxt/image"
-    ],
-      css: [
-        '@/resources/main.css',
-        '@/resources/scrollbars.css',
+  modules: [
+    '@sidebase/nuxt-auth',
+    '@nuxt/content',
+    'nuxt-windicss',
+    'nuxt-headlessui',
+    'nuxt-icon',
+    '@nuxt/image-edge',
+    '@formkit/nuxt',
+    "@nuxt/image"
+  ],
+  css: [
+    '@/resources/main.css',
+    '@/resources/scrollbars.css',
+  ],
+  image: {
+    cloudinary: {
+      baseURL: 'https://res.cloudinary.com/dpub6gcei/image/upload/v1678918300/GBstreams',
+      modifiers: {
+        quality: 'auto:best',
+        dpr: 'auto',
+      },
+    }
+  },
+  headlessui: {
+    prefix: 'Headless',
+  },
+  content: {
+    navigation: {
+      fields: ['icon'],
+    },
+    highlight: {
+      preload: [
+        'c',
+        'cpp',
+        'java',
       ],
-      image: {
-        cloudinary: {
-          baseURL: 'https://res.cloudinary.com/dpub6gcei/image/upload/v1678918300/GBstreams',
-          modifiers: {
-            quality: 'auto:best',
-            dpr: 'auto',
-          },
-        }
+      // Theme used in all color schemes.
+      theme: 'github-dark',
+    },
+    // documentDriven: true,
+    markdown: {
+      toc: {
+        depth: 4,
+        searchDepth: 4,
       },
-      headlessui: {
-        prefix: 'Headless',
-      },
-      content: {
-        navigation: {
-          fields: ['icon'],
-        },
-        highlight: {
-          preload: [
-            'c',
-            'cpp',
-            'java',
-          ],
-          // Theme used in all color schemes.
-          theme: 'github-dark',
-        },
-        // documentDriven: true,
-        markdown: {
-          toc: {
-            depth: 4,
-            searchDepth: 4,
-          },
-        },
-      },
+    },
+  },
 })
