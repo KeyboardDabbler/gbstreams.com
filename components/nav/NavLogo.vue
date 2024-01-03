a4db79aad8322fd791e46892231d91fe56a3d07a
keyboarddabbler <keyboarddabbler@gmail.com>
2024-01-03 22:04:57 +1300

    refactor: :art: Move logo into a component

---
+++ components/nav/NavLogo.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: {
+    imageClass: String,
+  },
+}
+</script>
+
+<template>
+    <NuxtLink>
+        <NuxtImg provider="cloudinary" src="/branding/banner-light_xgac6g.png" alt="GBstreams logo" :class="imageClass" />
+    </NuxtLink>
+</template>
