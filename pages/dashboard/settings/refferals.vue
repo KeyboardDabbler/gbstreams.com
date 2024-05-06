810de967db5844b75c0e62478dc96169bcd9f905
keyboarddabbler <keyboarddabbler@gmail.com>
2024-05-06 17:38:35 +1200

    feat: :sparkles: Add user refferals by invite code.

--- pages/dashboard/settings/refferals.vue
+++ pages/dashboard/settings/refferals.vue
@@ -1,19 +1,130 @@
 <script setup lang="ts">
-definePageMeta({
-  layout: "dashboard",
-});
+import { useAuthStore } from '~/store/auth'
+import { storeToRefs } from 'pinia'
+
+const store = useAuthStore()
+const { userDetails, referralDetails } = storeToRefs(store)
+
+const isInviteModalOpen = ref(false)
+
+const formattedExpiry = computed(() => {
+  const expiryTimestamp = referralDetails.value.expiry * 1000; // Convert seconds to milliseconds
+  const expiryDate = new Date(expiryTimestamp);
+  const options = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true
+  };
+  return expiryDate.toLocaleString("en-US", options);
+})
+
+const copyToClipboard = () => {
+  const inputElement = document.querySelector('input') as HTMLInputElement;
+  if (inputElement) {
+    inputElement.select();
+    document.execCommand('copy');
+  }
+}
+
 </script>
 
 <template>
-  <main class="container px-4 focus:outline-none">
-    <div class="mb-6">
-      <div class="max-w-8xl mx-auto px-4">
-        <NavProfileHeader :button="'View Profile'" :buttonLink="'/dashboard/settings/general'" />
-        <NavProfileSettings />
-        <section>
-          <ProfileReferrals />
-        </section>
-      </div>
+  <UDashboardPanelContent class="pb-24">
+    <div v-if="referralDetails && referralDetails.remaining_uses !== null && referralDetails.remaining_uses > 0">
+    <UDashboardSection
+      title="Refferals"
+      description="Invite friends and family to join GBstreams. Invitations are limited, so be sure to check back here for updates."
+    >
+      <UPageGrid :ui="{ wrapper: 'xl:grid-cols-2' }">
+        <UPageCard>
+          <div class="flex flex-col justify-center items-center">
+            <p class="text-2xl font-bold dark:text-gray-200 flex items-center">
+              {{ referralDetails.remaining_uses }}
+            </p>
+            <p class="font-medium dark:text-gray-200 flex items-center mt-2">
+              Remaining Invitations
+            </p>
+            <p class=" dark:text-gray-400 text-sm mt-1">
+              Number of invitations.
+            </p>
+          </div>
+        </UPageCard>
+        <UPageCard>
+          <div class="flex flex-col justify-center items-center">
+            <p class="text-2xl font-bold dark:text-gray-200 flex items-center">
+              {{ formattedExpiry }}
+            </p>
+            <p class="font-medium dark:text-gray-200 flex items-center mt-2">
+              Expiry Date
+            </p>
+            <p class="dark:text-gray-400 text-sm mt-1">
+              Invitation expiry date.
+            </p>
+          </div>
+        </UPageCard>
+      </UPageGrid>
+
+      <UFormGroup
+        label="Send Email Invitation"
+        description="Easily send invitations via email to friends or family!"
+        class="grid grid-cols-2 gap-2"
+        :ui="{ container: '' }"
+      >
+        <UButton
+          block
+          label="Send email"
+          color="black"
+          @click="isInviteModalOpen = true"
+        />
+      </UFormGroup>
+
+      <UFormGroup
+        label="Share Your Unique Code"
+        description="Copy your unique code and share it with friends or family!"
+        class="grid grid-cols-2 gap-2"
+        :ui="{ container: '' }"
+      >
+        <UButtonGroup
+          size="md"
+          class="mt-2"
+          orientation="horizontal"
+          :ui="{ wrapper: { horizontal: 'flex -space-x-px' } }"
+        >
+          <UInput 
+            v-model="referralDetails.code"
+            disabled
+            :ui="{ wrapper: 'w-full' }"
+          />
+          <UButton
+            icon="i-heroicons-clipboard-document"
+            color="gray"
+            @click="copyToClipboard"
+          />
+        </UButtonGroup>
+      </UFormGroup>
+    </UDashboardSection>
+
+    <UDashboardModal
+      v-model="isInviteModalOpen"
+      title="Invite people"
+      description="Invite new members to GBstreams by email address. Simply enter their email and we'll send them an invitation on your behalf."
+      :ui="{ width: 'sm:max-w-md' }"
+    >
+      <SettingsRefferalForm @close="isInviteModalOpen = false" />
+    </UDashboardModal>
+
     </div>
-  </main>
+
+    <div v-else>
+      <UPageHero 
+    align="center"
+    title="Referrals"
+    description="You've used all your invitations for now. Check back soon for more opportunities to invite friends and family to join GBstreams!"
+  />
+    </div>
+
+  </UDashboardPanelContent>
 </template>
--- store/auth.ts
+++ store/auth.ts
@@ -5,10 +5,34 @@ interface UserPayloadInterface {
   password: string
 }
 
+interface UserDetails {
+  id: string
+  username: string
+  expiry: number
+  admin: boolean
+  accounts_admin: boolean
+  disabled: boolean
+  email: {
+    value: string
+    enabled: boolean
+  }
+  has_referrals: boolean
+}
+
+interface ReferralDetails {
+  code: string
+  remaining_uses: number
+  no_limit: boolean
+  expiry: number
+  use_expiry: boolean
+}
+
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     authenticated: false,
-    loading: false
+    loading: false,
+    userDetails: [],
+    referralDetails: [],
   }),
   actions: {
     async authenticateUser({ username, password }: UserPayloadInterface) {
@@ -27,12 +51,44 @@ export const useAuthStore = defineStore('auth', {
         const token = useCookie('token') // useCookie new hook in nuxt 3
         token.value = data?.value?.token // set token to cookie
         this.authenticated = true // set authenticated  state value to true
+        await this.fetchUserDetails(token.value)
+        await this.fetchReferralDetails(token.value)
+      }
+    },
+    async fetchUserDetails(token: string) {
+      const apiUrl = useRuntimeConfig().public.JFA_URL
+
+      const { data }: any = await useFetch(`${apiUrl}my/details`, {
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      if (data.value) {
+        this.userDetails = data.value
+      }
+    },
+    async fetchReferralDetails(token: string) {
+      const apiUrl = useRuntimeConfig().public.JFA_URL
+
+      const { data }: any = await useFetch(`${apiUrl}my/referral`, {
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      if (data.value) {
+        this.referralDetails = data.value
       }
     },
     logUserOut() {
       const token = useCookie('token') // useCookie new hook in nuxt 3
       this.authenticated = false // set authenticated  state value to false
       token.value = null // clear the token cookie
+      this.userDetails = null
+      this.referralDetails = null
     }
   },
   persist: true
