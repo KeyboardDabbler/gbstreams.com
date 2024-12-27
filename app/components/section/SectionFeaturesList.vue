1eae8e2044a375393b3264a6d634cbe9c812dcec
keyboarddabbler <keyboarddabbler@gmail.com>
2024-12-28 11:11:39 +1300

    feat: SectionFeaturesList

---
+++ app/components/section/SectionFeaturesList.vue
@@ -0,0 +1,83 @@
+<template>
+    <section class="p-6">
+      <div class="container px-2 mx-auto">
+        <div class="md:max-w-4xl mb-4 mx-auto text-center">
+          <span
+            class="inline-block py-px px-2 mb-4 text-xs leading-5 text-yellow-1 bg-forg font-medium uppercase rounded-full shadow-sm">Features</span>
+          <h1 class="mb-4 text-gray-200 text-3xl md:text-4xl leading-tight font-bold tracking-tighter">What We Do</h1>
+          <p class="text-lg md:text-xl font-medium">Enjoy your favorite shows your
+            way, There's always something great on <span class="text-yellow-1 font-bold">GBstreams</span></p>
+        </div>
+
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div class="h-full p-8 text-center rounded-md">
+              <div class="inline-flex mb-6 mx-auto items-center justify-center">
+                <UIcon name="i-custom-featstreaming" class="size-32" />
+              </div>
+              <h3 class="mb-4 text-gray-200 text-xl md:text-2xl leading-tight font-bold">Streaming library</h3>
+              <p class="font-medium">Watch full seasons of exclusive series, classic favorites, Network Originals, hit
+                movies, current episodes, kids shows, and tons more..
+              </p>
+            </div>
+          </div>
+
+          <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div class="h-full p-8 text-center rounded-md">
+              <div class="inline-flex mb-6 mx-auto items-center justify-center">
+                <UIcon name="i-custom-featwatch" class="size-32" />
+              </div>
+              <h3 class="mb-4 text-gray-200 text-xl md:text-2xl leading-tight font-bold">Watch anywhere</h3>
+              <p class="font-medium">Enjoy from the web or with the GBstreams app on your phone, tablet, or select Smart
+                TVs — on up to 2 devices at once.</p>
+            </div>
+          </div>
+
+          <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div class="h-full p-8 text-center rounded-md">
+              <div class="inline-flex mb-6 mx-auto items-center justify-center">
+                <UIcon name="i-custom-featiptv" class="size-32" />
+              </div>
+              <h3 class="mb-4 text-gray-200 text-xl md:text-2xl leading-tight font-bold">IPTV</h3>
+              <p class="font-medium">Catch your games at home or on the go. Stream live games from major leagues from
+                around the world.</p>
+            </div>
+          </div>
+
+          <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div class="h-full p-8 text-center rounded-md">
+              <div class="inline-flex mb-6 mx-auto items-center justify-center">
+                <UIcon name="i-custom-featauto" class="size-32" />
+              </div>
+              <h3 class="mb-4 text-gray-200 text-xl md:text-2xl leading-tight font-bold">Automated services</h3>
+              <p class="font-medium">Request & reporting interface that is incredibly easy to understand and use.
+                Several
+                notification agents are directly supported</p>
+            </div>
+          </div>
+
+          <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div class="h-full p-8 text-center rounded-md">
+              <div class="inline-flex mb-6 mx-auto items-center justify-center">
+                <UIcon name="i-custom-featcdn" class="size-32" />
+              </div>
+              <h3 class="mb-4 text-gray-200 text-xl md:text-2xl leading-tight font-bold">CDN</h3>
+              <p class="font-medium">We store high demand content on high avaliable 10Gbps storage
+                to bring GBstreams faster load, seek, skip and play times.</p>
+            </div>
+          </div>
+
+          <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div class="h-full p-8 text-center rounded-md">
+              <div class="inline-flex mb-6 mx-auto items-center justify-center">
+                <UIcon name="i-custom-featfaq" class="size-32" />
+              </div>
+              <h3 class="mb-4 text-gray-200 text-xl md:text-2xl leading-tight font-bold">Fast support</h3>
+              <p class="font-medium">Access to live messaging, FAQ and helpful guides!!</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+</template>
