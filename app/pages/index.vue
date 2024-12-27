7cecb9ac16a4f97cb33aa04686365a31b2f0ddd0
keyboarddabbler <keyboarddabbler@gmail.com>
2024-12-28 12:48:49 +1300

    feat: add index

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+        <NavHeader />
+        <div class="flex-1 w-full flex flex-col">
+            <div class="relative flex-1 flex flex-col mx-auto max-w-8xl w-full h-full">
+                <SectionFeaturesList />
+            </div>
+        </div>
+        <NavFooter />
+    </div>
+</template>
