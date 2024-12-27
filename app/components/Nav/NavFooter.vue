842712990a8489315214cd153dd609566ab1a65e
keyboarddabbler <keyboarddabbler@gmail.com>
2024-12-28 12:48:01 +1300

    feat: add NavFooter

---
+++ app/components/Nav/NavFooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer>
+      <section class="container px-4 py-2 mx-auto border-t-1 border-gray-400 border-opacity-40">
+        <div class="row">
+          <div class="text-center text-sm text-gray-400">
+            <p class="my-3">Made with ❤️ by <NuxtLink to="https://github.com/KeyboardDabbler" class="text-yellow-1 font-bold">keyboardDabbler</NuxtLink></p>
+            <p class="my-3">© 2016-PRESENT <span class="text-yellow-1 font-bold">GBstreams</span> | All rights reserved.</p>
+          </div>
+        </div>
+      </section>
+    </footer>
+  </template>
