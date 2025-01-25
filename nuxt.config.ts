b3eb617ff2e34ea548833ae02318b7792e42a5c7
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-25 14:27:01 +1300

    Chore: add @nuxt/content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui-pro', '@nuxt/eslint', '@nuxt/image'],
+  modules: ['@nuxt/ui-pro', '@nuxt/content', '@nuxt/eslint', '@nuxt/image'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
 
