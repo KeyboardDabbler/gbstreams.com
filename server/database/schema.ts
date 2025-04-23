84286f6a016e836859ff55e23a0f96fb7a9c3d64
keyboarddabbler <keyboarddabbler@gmail.com>
2025-04-23 19:39:39 +1200

    feat: init Drizzle ORM

---
+++ server/database/schema.ts
@@ -0,0 +1,11 @@
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
+
+export const jellyfinUsers = sqliteTable('users', {
+  id: text('id').primaryKey(),
+  userName: text('user_name').notNull(),
+  avatarUrl: text('avatar_url'),
+  enableAutoLogin: integer('enable_auto_login', { mode: 'boolean' }).notNull(),
+  lastLoginDate: text('last_login_date'),
+  lastActivityDate: text('last_activity_date'),
+  accessToken: text('access_token')
+})
