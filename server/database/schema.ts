599d4672bcf276666869808c13965874cdf7bf82
keyboarddabbler <keyboarddabbler@gmail.com>
2025-04-25 18:29:56 +1200

    database: update tables

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -3,7 +3,9 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 export const jellyfinUsers = sqliteTable('users', {
   id: text('id').primaryKey(),
   userName: text('user_name').notNull(),
-  avatarUrl: text('avatar_url'),
+  email: text('email'),
+  avatar: text('avatar'),
+  bio: text('bio'),
   enableAutoLogin: integer('enable_auto_login', { mode: 'boolean' }).notNull(),
   lastLoginDate: text('last_login_date'),
   lastActivityDate: text('last_activity_date'),
