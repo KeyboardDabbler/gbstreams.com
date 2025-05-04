37fd9b5721d0da2d00d7f34eace77ac777ecb057
keyboarddabbler <keyboarddabbler@gmail.com>
2025-05-04 20:20:40 +1200

    fix: add hero section to home if user not a subscribed

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -4,26 +4,48 @@ definePageMeta({
 })
 
 const { isNotificationsSlideoverOpen } = useDashboard()
+
+const links = ref([
+  {
+    label: 'Complete Subscription',
+    to: '/dashboard/profile/billing',
+    color: 'neutral',
+    variant: 'subtle',
+    trailingIcon: 'i-lucide-arrow-right'
+  }
+])
 </script>
 
 <template>
   <UDashboardPanel id="home">
     <template #header>
-      <UDashboardNavbar title="Home" :ui="{ right: 'gap-3' }">
+      <UDashboardNavbar
+        title="Home"
+        :ui="{ right: 'gap-3' }"
+      >
         <template #leading>
           <UDashboardSidebarCollapse />
         </template>
 
         <template #right>
-          <UTooltip text="Notifications" :shortcuts="['N']">
+          <UTooltip
+            text="Notifications"
+            :shortcuts="['N']"
+          >
             <UButton
               color="neutral"
               variant="ghost"
               square
               @click="isNotificationsSlideoverOpen = true"
             >
-              <UChip color="error" inset>
-                <UIcon name="i-lucide-bell" class="size-5 shrink-0" />
+              <UChip
+                color="error"
+                inset
+              >
+                <UIcon
+                  name="i-lucide-bell"
+                  class="size-5 shrink-0"
+                />
               </UChip>
             </UButton>
           </UTooltip>
@@ -32,9 +54,14 @@ const { isNotificationsSlideoverOpen } = useDashboard()
     </template>
 
     <template #body>
-      <HomeStats />
-      <HomeChart />
-      <HomeLatest />
+      <UPageSection
+        title="You're one step closer to streaming"
+        description="Don't miss out on great movies and series. Explore the latest releases and find your next favorite."
+        icon="i-custom-gbstreams-stacked"
+        orientation="horizontal"
+        :links="links"
+        :ui="{ leadingIcon: 'size-32' }"
+      />
     </template>
   </UDashboardPanel>
 </template>
