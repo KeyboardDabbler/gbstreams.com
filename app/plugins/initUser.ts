71bb1233928c6d30d66c72268cd81b2f55f8da03
keyboarddabbler <keyboarddabbler@gmail.com>
2025-05-05 16:51:46 +1200

    fix: keep userStore hydrated with the latest user data whenever the session state changes

--- app/plugins/initUser.ts
+++ app/plugins/initUser.ts
@@ -1,8 +1,12 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
   const userStore = useUserStore()
   const { loggedIn } = useUserSession()
-  if (loggedIn.value) {
-    const { data } = await useFetch('/api/me')
-    if (data.value) userStore.setUser(data.value)
-  }
+  watchEffect(async () => {
+    if (loggedIn.value) {
+      const { data } = await useFetch('/api/me')
+      if (data.value) userStore.setUser(data.value)
+    } else {
+      userStore.clearUser()
+    }
+  })
 })
